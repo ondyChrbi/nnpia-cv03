2273b5c10345bc46e3e72eb2f699789e042ba554
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-02-27 15:42:33 +0100

    Clean code, který není v zadání.

--- src/main/java/cz/upce/fe/cv02/controller/AppUserController.java
+++ src/main/java/cz/upce/fe/cv02/controller/AppUserController.java
@@ -1,6 +1,7 @@
 package cz.upce.fe.cv02.controller;
 
 import cz.upce.fe.cv02.domain.AppUser;
+import cz.upce.fe.cv02.dto.AppUserResponseDtoV1;
 import cz.upce.fe.cv02.service.AppUserService;
 import cz.upce.fe.cv02.service.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/app-user")
@@ -19,16 +21,32 @@ public class AppUserController {
     private final AppUserService appUserService;
 
     @GetMapping("")
-    public ResponseEntity<List<AppUser>> findAll() {
+    public ResponseEntity<List<AppUserResponseDtoV1>> findAll() {
         var result = appUserService.findAllByActiveEquals();
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(result
+                .stream()
+                .map(AppUserController::toDto)
+                .collect(Collectors.toList())
+        );
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findById(@PathVariable final Long id) throws ResourceNotFoundException {
         var result = appUserService.findById(id);
 
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(toDto(result));
+    }
+
+    private static AppUserResponseDtoV1 toDto(final AppUser appUser) {
+        return new AppUserResponseDtoV1(
+                appUser.getId(),
+                appUser.getUsername(),
+                appUser.getPassword(),
+                appUser.getActive(),
+                appUser.getCreationDate(),
+                appUser.getUpdateDate(),
+                appUser.getRoles()
+        );
     }
 }
